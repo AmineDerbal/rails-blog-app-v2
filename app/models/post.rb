3f997dec5c5a906fd9d295778d09f9f5802bd196
Amine derbal <aminedoran31@gmail.com>
2023-06-06 17:22:10 +0100

    add validations to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: :post_id
   has_many :likes, class_name: 'Like', foreign_key: :post_id
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true
+  validates :comments_counter , numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counters , numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
   after_create :update_user_posts_counter
 
   def recent_comments
