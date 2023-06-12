1564a79f78513475ae266f40492b8b5a23607c7d
Amine derbal <aminedoran31@gmail.com>
2023-06-12 13:12:22 +0100

    Create spec_tests for posts

---
+++ spec/requests/posts_request_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/users/15/posts'
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+      expect(response).to render_template('index')
+      expect(response.body).to include('list of all posts and comment of a user')
+
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users/20/posts/1'
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+      expect(response).to render_template('show')
+      expect(response.body).to include('details of a post')
+    end
+  end
+  end
