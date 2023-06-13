d98c9b6ceef8b22bd72110e9cd9d5c8f669d5c23
Amine derbal <aminedoran31@gmail.com>
2023-06-13 11:33:31 +0100

    Update files to fix linters errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,10 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
-    
   end
 
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
-   end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-
   layout 'standard'
 
   def index
-      @users = User.all
-   end
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
-   end
+  end
 end
