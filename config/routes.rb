1d5eea8fe32901fcd87234487c2530bccdf6cef2
Amine derbal <aminedoran31@gmail.com>
2023-06-14 12:59:07 +0100

    Add route for like in posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do 
       resources :comments, only: [:create, :new]
+      post 'like', on: :member
     end  
  end
 
