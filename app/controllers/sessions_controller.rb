37388ea8c81d42c6999785910beda06da1f2c572
Faranosh Amini <faranosh.amini4@gmail.com>
2023-06-20 04:20:01 +0430

    Create sessions_controller.rb

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < Devise::SessionsController
+    def destroy
+      sign_out(current_user)
+      redirect_to root_path, notice: 'You have been signed out successfully.'
+    end
+  end
+  
