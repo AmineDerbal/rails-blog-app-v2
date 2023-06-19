ed0b0904945faf90b871078070ad5aeabcd30a13
Faranosh Amini <faranosh.amini4@gmail.com>
2023-06-20 04:19:22 +0430

    Create registrations_controller.rb

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+class RegistrationsController < Devise::RegistrationsController
+    private
+  ​
+    def sign_up_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :photo, :bio, :posts_counter)
+    end
+  end
+  
