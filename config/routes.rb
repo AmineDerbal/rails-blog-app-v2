c067ed2a973fd8d2dc72ff79e3cc859546f45c0d
Amine derbal <aminedoran31@gmail.com>
2023-06-20 15:37:53 +0100

    Add custom routes for login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  get 'home/index'
 
+  # devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' , sessions: 'sessions' }
+
+  # Add the following route for signing out
+  delete '/sign_out', to: 'sessions#destroy', as: :custom_destroy_user_session
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :custom_login
+    post 'login', to: 'devise/sessions#create', as: :custom_user_session
+  
+    get 'registration', to: 'devise/registrations#new', as: :custom_registration
+    post 'registration', to: 'devise/registrations#create', as: :custom_user_registration
+  end
+
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do 
       resources :comments, only: [:create, :new]
@@ -7,7 +23,7 @@ Rails.application.routes.draw do
     end  
  end
 
-  root 'users#index'
+ root 'home#index'
 
  
 
