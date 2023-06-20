d8444acd2cc092886b7e40307148f9b5f2c0060f
Amine derbal <aminedoran31@gmail.com>
2023-06-20 19:54:57 +0100

    Fix linter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-
 end
