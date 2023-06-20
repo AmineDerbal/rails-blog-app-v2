e634a544b9f15ff05c23585d170159f809bff42a
Amine derbal <aminedoran31@gmail.com>
2023-06-20 16:53:23 +0100

    Setup sign out logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
   layout 'standard'
+  before_action :authenticate_user!
 
   def index
+    @user = current_user
     @users = User.all
   end
-
+  
   def show
-    @user = User.find(params[:id])
-    @first_three_posts = @user.posts.limit(3)
-    @recent_posts = @user.recent_posts
+    if params[:id] == 'sign_out'
+      sign_out(current_user)
+      redirect_to root_path, notice: 'You have been signed out successfully.'
+    else
+      @user = User.find(params[:id])
+      @first_three_posts = @user.posts.limit(3)
+      @recent_posts = @user.recent_posts
+    end
   end
 end
+
