1a23355f2d0cfa6dfafaa977d006371820e2a229
Faranosh Amini <faranosh.amini4@gmail.com>
2023-06-21 06:00:29 +0430

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
   layout 'standard'
   before_action :authenticate_user!
-
+​
   def index
     @user = current_user
     @users = User.all
   end
-
+​
   def show
     if params[:id] == 'sign_out'
       sign_out(current_user)
       redirect_to root_path, notice: 'You have been signed out successfully.'
     else
-      @user = User.find(params[:id])
+       @current_user = current_user
+       @user = User.find(params[:id])
       @first_three_posts = @user.posts.limit(3)
       @recent_posts = @user.recent_posts
     end
