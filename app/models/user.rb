205a3c9f577437816fa6f27051aa3d7215fe4701
Amine derbal <aminedoran31@gmail.com>
2023-06-21 15:29:46 +0100

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,11 @@ class User < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: :author_id
   validates :name, presence: true
   validates :photo, presence: true
-  validates :bio, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def admin?
+    role == 'admin'
+  end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
