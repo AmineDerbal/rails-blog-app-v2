2809d4660b755fc1ed9ba6252fce51e52642c5b3
Amine derbal <aminedoran31@gmail.com>
2023-06-21 15:30:42 +0100

    Add decrement method after comment is deleted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,8 +5,14 @@ class Comment < ApplicationRecord
   validates :text, presence: true
 
   after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter_after_destroy
 
   def update_post_comments_counter
     post.increment!(:comments_counter)
   end
+
+  def update_post_comments_counter_after_destroy
+    post.decrement!(:comments_counter)
+end
+
 end
