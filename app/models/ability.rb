a5a4000171e287e1994677309c0bc37e7aae0997
Amine derbal <aminedoran31@gmail.com>
2023-06-21 17:10:33 +0100

    Add user and admin abilities

---
+++ app/models/ability.rb
@@ -0,0 +1,25 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user
+    can :read, Post, public: true
+
+    return unless user.present?  # additional permissions for logged in users (they can read their own posts)
+    can :read, Post, user: user
+    can :destroy, Post, author_id: user.id
+    can :destroy, Comment, author_id: user.id
+    can :destroy, Post, id: user.posts.pluck(:id) # Grant permission to destroy posts that belong to the user
+
+
+
+    return unless user.admin?  # additional permissions for administrators
+    can :read, Post
+    can :destroy, Post, author_id: user.id
+    can :destroy, Comment, author_id: user.id
+
+    return if user.role != 'admin'  # only administrators have the following permissions
+    can :destroy, Post
+    can :destroy, Comment
+  end
+end
