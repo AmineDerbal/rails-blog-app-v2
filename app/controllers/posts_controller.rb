848bec7012d3f4446a08165b115546e87f95f7e4
Faranosh Amini <faranosh.amini4@gmail.com>
2023-06-22 20:34:18 +0430

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,60 +1,14 @@
-class PostsController < ApplicationController
-  layout 'standard'
-  before_action :authenticate_user!
-
+class Api::PostsController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :authenticate_user!
+​
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
-    @current_user = current_user
-  end
-
-  def show
-    @post = Post.find(params[:id])
-    @user = User.find(params[:user_id])
-    @current_user = current_user
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def like
-    @post = Post.find(params[:id])
-    @user = current_user
-    @like = Like.new(author_id: current_user.id, post_id: @post.id)
-    @user_id = params[:user_id]
-    @like.save
-    redirect_to user_post_path(user_id: @user_id, id: params[:id])
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-
-    # Destroy all comments
-    @post.comments.destroy_all if @post.comments.exists?
-    # Destroy all likes
-    @post.likes.destroy_all if @post.likes.exists?
-
-    @post.destroy
-    redirect_to user_posts_path(id: current_user.id)
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.author_id = current_user.id
-    @post.comments_counter = 0
-    @post.likes_counters = 0
-
-    if @post.save
-      redirect_to user_posts_path, notice: 'Post created successfully'
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :text)
+    user = User.find(params[:user_id])
+    posts = user.posts
+    render json: posts
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'User not found' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 end
